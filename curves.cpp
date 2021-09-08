a7644a9bb072b45ea8b663e2fc00286d0895b336
clk1006 <casparkiehn@icloud.com>
2021-09-08 16:55:42 +0200

    Addition von zwei Punkten fertiggestellt, nicht getestet

--- curves.cpp
+++ curves.cpp
@@ -23,11 +23,17 @@ bool eCurve::isOn(point p){
 point eCurve::add(point p1,point p2){
     point pEnd;
     if(!(isOn(p1)&&isOn(p2))){
-        pEnd.x=NULL;
-        pEnd.y=NULL;
         return pEnd;
     }
     double mSec=(p2.y-p1.y)/(p2.x-p1.x);
     double bSec=p1.y-mSec*p1.x;
     double polyn[4]={1,-pow(mSec,2),a-2*mSec*bSec,b-pow(bSec,2)};
+    double zeros[3]={getZeros(polyn)[0],getZeros(polyn)[1],getZeros(polyn)[2]};
+    for(int i=0;i<3;i++){
+        if(zeros[i]!=NULL&&zeros[i]!=p1.x&&zeros[i]!=p2.x){
+            pEnd.x=zeros[i];
+            pEnd.y=-(mSec*pEnd.x+bSec);
+        }
+    }
+    return pEnd;
 }
