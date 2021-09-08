1419f47b0ea5e5b48592db889e020f7eba252ac0
clk1006 <casparkiehn@icloud.com>
2021-09-08 20:34:31 +0200

    multiplikationen hinzugefügt, Definition der elliptischen Kurven sollte fertg sein

--- curves.cpp
+++ curves.cpp
@@ -25,15 +25,35 @@ point eCurve::add(point p1,point p2){
     if(!(isOn(p1)&&isOn(p2))){
         return pEnd;
     }
-    double mSec=(p2.y-p1.y)/(p2.x-p1.x);
-    double bSec=p1.y-mSec*p1.x;
+    const double mSec=(p2.y-p1.y)/(p2.x-p1.x);
+    const double bSec=p1.y-mSec*p1.x;
     double polyn[4]={1,-pow(mSec,2),a-2*mSec*bSec,b-pow(bSec,2)};
-    double zeros[3]={getZeros(polyn)[0],getZeros(polyn)[1],getZeros(polyn)[2]};
+    const double zeros[3]={getZeros(polyn)[0],getZeros(polyn)[1],getZeros(polyn)[2]};
     for(int i=0;i<3;i++){
         if(zeros[i]!=NULL&&zeros[i]!=p1.x&&zeros[i]!=p2.x){
             pEnd.x=zeros[i];
             pEnd.y=-(mSec*pEnd.x+bSec);
         }
+    }
+    return pEnd;
+}
+point eCurve::multiply(point p, int factor){
+    if(factor==1) return p;
+    if(factor==0) return {x:NULL,y:NULL};
+    point pEnd;
+    const double slope=1/2*(3*pow(p.x,2)-2)/(sqrt(pow(p.x,3)-2*p.x+2));
+    const double mTan=p.y>0?slope:-slope;
+    const double bTan=p.y-mTan*p.x;
+    double polyn[4]={1,-pow(mTan,2),a-2*mTan*bTan,b-pow(bTan,2)};
+    const double zeros[3]={getZeros(polyn)[0],getZeros(polyn)[1],getZeros(polyn)[2]};
+    for(int i=0;i<3;i++){
+        if(zeros[i]!=NULL&&zeros[i]!=p.x){
+            pEnd.x=zeros[i];
+            pEnd.y=-(mTan*pEnd.x+bTan);
+        }
+    }
+    for(int i=3;i<=factor;i++){
+        pEnd=add(pEnd,p);
     }
     return pEnd;
 }
