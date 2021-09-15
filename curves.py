fbe6646971a696bb434f1fda491450f3fffa86e9
clk1006 <46888928+clk1006@users.noreply.github.com>
2021-09-15 11:39:30 +0200

    Algorithmus hinzugefügt, um n zu ermitteln

--- curves.py
+++ curves.py
@@ -4,15 +4,19 @@ class point:
     def __init__(self,x,y):
         self.x=x
         self.y=y
+    def __ne__(self,other):
+        return self.x!=other.x or self.y!=other.y
+    def __eq__(self,other):
+        return self.x==other.x and self.y==other.y
 class eCurve:
     def __init__(self,a,b,order):
         self.a=a
         self.b=b
         self.order=order
     def getPoint(self,x,isBigger):
-        res=(x**2+self.a*x+self.b)%self.order
+        res=(x**3+self.a*x+self.b)%self.order
         y=-1
-        for i in range((self.order-1)/2):
+        for i in range(int((self.order+1)/2)):
             if (i**2)%self.order==res:
                 y=i
                 break
@@ -21,26 +25,26 @@ class eCurve:
         if isBigger:
             return point(x,self.order-y)
         return point(x,y)
-    def check(self,p):
-        return p.y**2==p.x**3+self.a*p.x+self.b
     def add(self,p,q):
-        pass
+        if p==point(0,0):
+            return q
+        if q==point(0,0):
+            return p
+        if p.x==q.x and p.y==self.order-q.y:
+            return point(0,0)
+        lam=0
+        if p.x==q.x and p.y==q.y:
+            lam=(3*p.x**2+self.a)/(2*p.y)
+        else:
+            lam=(p.y-q.y)/(p.x-q.x)
+        x=-p.x-q.x+lam**2
+        if x!=int(x):
+            return point(0,0)
+        y=-p.y+lam*(p.x-x)
+        return point(x%self.order,y%self.order)
     def multiply(self,p,factor):
         if factor==0:
-            return NoReturn
+            return point(0,0)
         if factor==1:
             return p
-        if factor==2:
-            return self.getDouble(p)
         return self.add(self.multiply(p,factor-1),p)
-    def getDouble(self,p):
-        q=point(0,0)
-        for i in range(self.order):
-            if i!=p.x and self.getPoint(i,False)!=NoReturn:
-                q=self.getPoint(i,False)
-        p2=self.add(p,q)
-        p3=self.add(p,p2)
-        p3.y=self.order-p3.y
-        p4=self.add(p3,q)
-        p4.y=self.order-p4.y
-        return p4
--- ecdsa.py
+++ ecdsa.py
@@ -4,4 +4,6 @@ def getN(curve,p):
     while(curve.multiply(p,i)!=point(0,0)):
         i+=1
     return i
-print(point(0,0)==point(0,0))
+curve=eCurve(-3,5,19)
+p=curve.getPoint(3,False)
+print(getN(curve,p))
