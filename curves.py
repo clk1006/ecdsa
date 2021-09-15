55293375f5757f4c6658ba22653d18cf1b0d1bc7
clk1006 <casparkiehn@icloud.com>
2021-09-15 16:47:34 +0200

    Funktionen für ECDSA hinzugefügt

--- curves.py
+++ curves.py
@@ -1,5 +1,6 @@
 from math import *
 from typing import NoReturn
+from random import randint
 class point:
     def __init__(self,x,y):
         self.x=x
@@ -10,13 +11,17 @@ class point:
         return self.x==other.x and self.y==other.y
     def __str__(self):
         return "("+str(self.x)+"|"+str(self.y)+")"
+def getZ(m,n):
+    bitLen=int(log2(n))+1
+    e=hash(m)
+    if(e<0):
+        return int(str(bin(e))[3:bitLen+3],2)
+    return int(str(bin(e))[2:bitLen+2],2)
 class eCurve:
     def __init__(self,a,b,order):
         self.a=a
         self.b=b
         self.order=order
-    def isIdentity(self,p):
-        return p.x==0 and p.y==0
     def getPoint(self,x,isBigger):
         res=(x**3+self.a*x+self.b)%self.order
         y=-1
@@ -57,3 +62,35 @@ class eCurve:
         while(self.multiply(p,n)!=point(0,0)):
             n+=1
         return n
+    def createSignature(self,G,n,d,m):
+        z=getZ(m,n)
+        while True:
+            k=randint(1,n-1)
+            p=self.multiply(G,k)
+            r=p.x%n
+            if r==0:
+                continue
+            s=((z+r*d)/k)%n
+            if s==0:
+                continue
+            return (r,s)
+    def checkSignature(self,G,n,Q,m,sig):
+        (r,s)=sig
+        if Q==point(0,0):
+            return False
+        if (Q.y**2)%self.order!=(Q.x**3+self.a*Q.x+self.b)%self.order:
+            return False
+        if self.multiply(Q,n)!=point(0,0):
+            return False
+        if not 0<r<n and 0<s<n:
+            return False
+        z=getZ(m,n)
+        u1=(z/s)%n
+        u2=(r/s)%n
+        p=self.add(self.multiply(G,u1),self.multiply(Q,u2))
+        if p==point(0,0):
+            return False
+        if r%n!=p.x%n:
+            return False
+        return True
+
